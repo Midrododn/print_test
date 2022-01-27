ec773f074aaf28c9040605de89ec072e6a4f7464
vbekasov <vbekasov@students.42wolfsburg.de>
2022-01-28 00:02:12 +0100

    tst

--- test.c
+++ test.c
@@ -5,9 +5,15 @@
 int main(void){
 
 	ft_printf("\n---------------------\n");
+	char	*ptr = NULL;
 
-	int tmp = ft_printf("%c", 'A');
-	int tmp2 = printf("%c", 'A');
+	int tmp = ft_printf("ft str=%s|\n", ptr);
+	int tmp2 = printf("pr str=%s|\n", ptr);
+	printf("\n!%d %d!\n ", tmp, tmp2);
+
+	tmp = ft_printf("ft ptr=%p|\n", ptr);
+	tmp2 = printf("pr ptr=%p|\n", ptr);
 	printf("\n%d %d\n ", tmp, tmp2);
+
 	return 0;
 }
